ad2961660e6a229b620c02d95d95c7b87515d087
Albert Zhan <albert.cfzhan@gmail.com>
2023-07-08 23:46:17 -0700

    Spawner

--- GMTK/Assets/Scripts/SpawnEnemies.cs
+++ GMTK/Assets/Scripts/SpawnEnemies.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private float initialSpawnRate;
+    [SerializeField] private float spawnRateChange;
+
+    private const float MinimumSpawnRate = 1.0f;
+    private float spawnRate;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        spawnRate = initialSpawnRate;
+        Spawn();
+        StartCoroutine(EnemySpawn());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator EnemySpawn()
     {
-        
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            Spawn();
+            
+            spawnRate -= spawnRateChange;
+            if (spawnRate < MinimumSpawnRate)
+            {
+                spawnRate = MinimumSpawnRate;
+            }
+        }
+    }
+
+    private void Spawn()
+    {
+        Vector3 newPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-5f, 5f), 0);
+        Instantiate(enemies[Random.Range(0, enemies.Count)], newPosition, transform.rotation);
     }
 }
