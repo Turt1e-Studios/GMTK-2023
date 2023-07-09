4e466dd414f37cd2b8868f6209fcf9c2ca297e05
Albert Zhan <albert.cfzhan@gmail.com>
2023-07-09 14:11:00 -0700

    Added time until enemies begin to shoot

--- GMTK/Assets/Scripts/Bullet/ShootBullet.cs
+++ GMTK/Assets/Scripts/Bullet/ShootBullet.cs
@@ -5,6 +5,7 @@ using Random = UnityEngine.Random;
 public class ShootBullet : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float timeUntilBeginShoot;
     [SerializeField] private float bulletMultiplier;
     [SerializeField] private float cooldown;
     [SerializeField] private bool isInSameDirection;
@@ -15,7 +16,7 @@ public class ShootBullet : MonoBehaviour
     private void Start()
     {
         player = GameObject.Find("Character");
-        InvokeRepeating(nameof(Shoot), 0.1f, cooldown);
+        InvokeRepeating(nameof(Shoot), timeUntilBeginShoot, cooldown);
     }
 
     void Shoot()
